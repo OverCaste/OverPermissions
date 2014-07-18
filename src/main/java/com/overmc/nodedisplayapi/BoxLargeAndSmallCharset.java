2e92309f9ab995d83a2c9fc328384a9104fca4a5
OverCaste <overcaste0@gmail.com>
2014-07-18 18:19:23 -0500

    Add a little API for making trees of text.

---
+++ src/main/java/com/overmc/nodedisplayapi/BoxLargeAndSmallCharset.java
@@ -0,0 +1,7 @@
+package com.overmc.nodedisplayapi;
+
+public class BoxLargeAndSmallCharset extends NodeDisplayCharset {
+    public BoxLargeAndSmallCharset( ) {
+        super('\u2550', '\u2551', '\u2554', '\u2557', '\u255A', '\u255D', '\u255F', '\u252C', '\u251C', '\u2514', '\u2500', '\u2502'); //Magic...
+    }
+}
---
+++ src/main/java/com/overmc/nodedisplayapi/ElementBoxNode.java
@@ -0,0 +1,49 @@
+package com.overmc.nodedisplayapi;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ElementBoxNode {
+    private final String name;
+    private final List<ElementBoxNode> subNodes;
+    private final List<String> lines;
+    
+    public ElementBoxNode(String name, List<ElementBoxNode> subNodes, List<String> lines) {
+        if(subNodes == null) {
+            this.subNodes = Collections.emptyList();
+        } else {
+            this.subNodes = new ArrayList<>(subNodes);
+        }
+        if(lines == null) {
+            this.lines = Collections.emptyList();
+        } else {
+            this.lines = new ArrayList<>(lines);
+        }
+        this.name = name;
+    }
+
+    public String getName( ) {
+        return name;
+    }
+
+    public List<ElementBoxNode> getSubNodes( ) {
+        return Collections.unmodifiableList(subNodes);
+    }
+
+    public List<String> getLines( ) {
+        return Collections.unmodifiableList(lines);
+    }
+    
+    public int size( ) {
+        int size = 0;
+        if(name != null) {
+            size += 1;
+        }
+        size += lines.size();
+        for(ElementBoxNode node : subNodes) {
+            size += node.size();
+        }
+        return size;
+    }
+}
